fb224fdb18968262f998ce3510785f63cca52bda
Caio Reis <caiofreis2005@gmail.com>
2025-02-18 12:52:08 -0300

    Adds solution for the TwoSum problem

---
+++ algorithms/TwoSum/TwoSum.py
@@ -0,0 +1,11 @@
+"""
+Solution for TwoSum
+"""
+
+class Solution:
+    def TwoSum(self, nums: List[int], target: int) -> List[int]:
+        hasher = {}
+        for idx, i in enumerate(nums):
+            if hasher.get(i) is not None:
+                return[hasher.get(i), idx]
+            hasher[target - i] = idx
